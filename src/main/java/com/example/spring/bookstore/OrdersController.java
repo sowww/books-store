f0084c52b29aaaceb407d1015894bb6ab60388aa
Yuri Kulikov <yuri.kulikov@akvelon.com>
2018-07-19 17:32:08 +0300

    Trying some things. Need to rework completely.

--- src/main/java/com/example/spring/bookstore/OrdersController.java
+++ src/main/java/com/example/spring/bookstore/OrdersController.java
@@ -1,12 +1,13 @@
 package com.example.spring.bookstore;
 
 
+import com.example.spring.bookstore.db.order.Order;
 import com.example.spring.bookstore.db.order.OrdersRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
@@ -20,15 +21,34 @@ public class OrdersController {
     private final OrdersRepository ordersRepository;
 
     public OrdersController(OrdersRepository ordersRepository) {
+        // Getting orders repo
         this.ordersRepository = ordersRepository;
     }
 
-    @PostMapping(value = "/{bookIds}/{userId}", produces = "application/json")
-    public void createNewOrder(@PathVariable ArrayList<Long> bookIds,
-                                       @PathVariable Long userId,
-                                       HttpServletResponse response) {
-        bookIds.forEach(id -> log.info("Order. Book with id: {}", id));
+    // JUST FOR TEST
+    // REWORK ALL OF THIS!!!
+
+    // Creating new order
+    // example: POST /api/orders?bookIds=1,2,5,3&userId=12
+    @PostMapping(value = "", produces = "application/json")
+    public Order createNewOrder(@RequestParam ArrayList<Long> bookIds,
+                                @RequestParam Long userId,
+                                HttpServletResponse response) {
+        log.info("User id from request:{}", userId);
+
+        float sum = 0f;
+        for (Long bookId : bookIds) {
+            log.info("Order. Book with id: {}", bookId);
+            sum += 100f;
+        }
         response.setStatus(HttpServletResponse.SC_ACCEPTED);
+
+        Order order = new Order(userId, sum, bookIds, Order.Status.PENDING);
+
         // TODO
+        return order;
     }
+
+    // JUST FOR TEST
+    // REWORK ALL OF THIS!!!
 }
