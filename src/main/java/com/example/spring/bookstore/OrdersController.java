76ee04cf4b0d9f4b570d35f235cea1b8a0f69bd0
Yuri Kulikov <yuri.kulikov@akvelon.com>
2018-07-20 12:02:03 +0300

    Added more requests

--- src/main/java/com/example/spring/bookstore/OrdersController.java
+++ src/main/java/com/example/spring/bookstore/OrdersController.java
@@ -1,8 +1,11 @@
 package com.example.spring.bookstore;
 
 
+import com.example.spring.bookstore.db.book.Book;
+import com.example.spring.bookstore.db.book.BooksRepository;
 import com.example.spring.bookstore.db.order.Order;
 import com.example.spring.bookstore.db.order.OrdersRepository;
+import com.example.spring.bookstore.db.user.UsersRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -17,14 +20,44 @@ public class OrdersController {
 
     private final Logger log = LoggerFactory.getLogger(OrdersController.class);
     private final OrdersRepository ordersRepository;
+    private final BooksRepository booksRepository;
+    private final UsersRepository usersRepository;
 
-    public OrdersController(OrdersRepository ordersRepository) {
+    public OrdersController(OrdersRepository ordersRepository,
+                            BooksRepository booksRepository,
+                            UsersRepository usersRepository) {
         // Getting orders repo
         this.ordersRepository = ordersRepository;
+        this.booksRepository = booksRepository;
+        this.usersRepository = usersRepository;
     }
 
-    // JUST FOR TEST
-    // REWORK ALL OF THIS!!!
+    private boolean isOrderCorrect(ArrayList<Long> bookIds, Long userId) {
+
+        for (Long bookId : bookIds) {
+            Optional<Book> book = booksRepository.findById(bookId);
+            if (!book.isPresent()) {
+                return false;
+            } else {
+                if (book.get().getCount() < 1) {
+                    return false;
+                }
+            }
+        }
+        if (!usersRepository.findById(userId).isPresent()) return false;
+
+        return true;
+    }
+
+    private float getBooksTotalPrice(ArrayList<Long> bookIds) {
+        float sum = 0;
+
+        for (Long bookId : bookIds) {
+            sum += booksRepository.findById(bookId).get().getPrice();
+        }
+
+        return sum;
+    }
 
     // Creating new order
     // example: POST /api/orders?bookIds=1,2,5,3&userId=12
@@ -34,32 +67,49 @@ public class OrdersController {
                                 HttpServletResponse response) {
         log.info("User id from request:{}", userId);
 
-        float sum = 0f;
-        for (Long bookId : bookIds) {
-            log.info("Order. Book with id: {}", bookId);
-            sum += 100f;
-        }
-        response.setStatus(HttpServletResponse.SC_ACCEPTED);
 
-        Order order = new Order(userId, sum, bookIds, Order.Status.PENDING);
-        ordersRepository.save(order);
+        if (isOrderCorrect(bookIds, userId)) {
+            float totalPrice = getBooksTotalPrice(bookIds);
+            Order order = new Order(userId, totalPrice, bookIds, Order.Status.PENDING);
+            ordersRepository.save(order);
+            response.setStatus(HttpServletResponse.SC_ACCEPTED);
+            return order;
+        }
 
         // TODO
-        return order;
+        return null;
     }
 
-    // JUST FOR TEST
-    // REWORK ALL OF THIS!!!
+    // Getting all orders
+    // example: GET /api/orders
+    @GetMapping(value = {"", "/"})
+    public Iterable<Order> getAllOrders() {
+        Iterable<Order> orders = ordersRepository.findAll();
+        log.info("Get all orders: {}", orders.spliterator().getExactSizeIfKnown());
+        return orders;
+    }
 
     // Getting order by Id
     // example: GET /api/orders/12
-    @GetMapping(value = "/{orderId}")
-    public Optional<Order> getOrderById(@PathVariable Long orderId,
-                                       HttpServletResponse response) {
-        log.info("Getting order by id: {}", orderId);
+    @GetMapping(value = "/{id}")
+    public Optional<Order> getOrderById(@PathVariable Long id,
+                                        HttpServletResponse response) {
+        log.info("Getting order by id: {}", id);
 
-        // TODO
-        return ordersRepository.findById(orderId);
+        // Getting order from repo
+        Optional<Order> order = ordersRepository.findById(id);
+
+        // If order doesn't exist
+        if (!order.isPresent()) {
+            log.info("Order with id:{} not found", id);
+            // Setting status to 404 and returning nothing
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            response.addHeader("message", "Order is not found");
+            return Optional.empty();
+        }
+
+        // else returning order
+        return order;
     }
 
     // Getting all orders with userId
