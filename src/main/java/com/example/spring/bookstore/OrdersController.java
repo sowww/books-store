fde104b29dfd296914f12c8ba45c223e95df13a6
Yuri Kulikov <yuri.kulikov@akvelon.com>
2018-07-20 13:05:42 +0300

    Added: - Many comments - deleteOrderById command

--- src/main/java/com/example/spring/bookstore/OrdersController.java
+++ src/main/java/com/example/spring/bookstore/OrdersController.java
@@ -26,36 +26,47 @@ public class OrdersController {
     public OrdersController(OrdersRepository ordersRepository,
                             BooksRepository booksRepository,
                             UsersRepository usersRepository) {
-        // Getting orders repo
+        // Getting repos
         this.ordersRepository = ordersRepository;
         this.booksRepository = booksRepository;
         this.usersRepository = usersRepository;
     }
 
+    // Checking if we can create order with this params
     private boolean isOrderCorrect(ArrayList<Long> bookIds, Long userId) {
-
+        // For every book id
         for (Long bookId : bookIds) {
+            // Getting the book
             Optional<Book> book = booksRepository.findById(bookId);
+            // If the book doesn't exists
             if (!book.isPresent()) {
+                // can't create order
                 return false;
             } else {
+                // if the book exists, checking books count
                 if (book.get().getCount() < 1) {
+                    // can't create order without book
                     return false;
                 }
             }
         }
+
+        // If the user doesn't exists, return false
         if (!usersRepository.findById(userId).isPresent()) return false;
 
+        // In other cases return true
         return true;
     }
 
+
+    // Getting sum of book prices
     private float getBooksTotalPrice(ArrayList<Long> bookIds) {
         float sum = 0;
-
+        // For every bookId
         for (Long bookId : bookIds) {
+            // Adding book price to sum
             sum += booksRepository.findById(bookId).get().getPrice();
         }
-
         return sum;
     }
 
@@ -65,17 +76,28 @@ public class OrdersController {
     public Order createNewOrder(@RequestParam ArrayList<Long> bookIds,
                                 @RequestParam Long userId,
                                 HttpServletResponse response) {
-        log.info("User id from request:{}", userId);
 
+        log.info("Trying to create order. bookIds:{} userId:{}", bookIds, userId);
 
+        // Checking data for order
         if (isOrderCorrect(bookIds, userId)) {
+
+            // If data is ok
+            log.info("Trying to create order. bookIds:{} userId:{}", bookIds, userId);
+            // Getting books total price
             float totalPrice = getBooksTotalPrice(bookIds);
+            // Creating new order
             Order order = new Order(userId, totalPrice, bookIds, Order.Status.PENDING);
+            // Saving it in repo
             ordersRepository.save(order);
+            // Response Created (201)
             response.setStatus(HttpServletResponse.SC_CREATED);
+            // Returning order
             return order;
         } else {
+            // If data is not ok, response with (400)
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            // And return null
             return null;
         }
     }
@@ -84,6 +106,7 @@ public class OrdersController {
     // example: GET /api/orders
     @GetMapping(value = {"", "/"})
     public Iterable<Order> getAllOrders() {
+        // Getting all orders from repo
         Iterable<Order> orders = ordersRepository.findAll();
         log.info("Get all orders: {}", orders.spliterator().getExactSizeIfKnown());
         return orders;
@@ -121,5 +144,27 @@ public class OrdersController {
 
         // TODO
         return ordersRepository.getOrdersByUserId(userId);
+    }
+
+    // Deleting order by id
+    // example: DELETE /api/orders/12
+    @DeleteMapping(value = "/{id}")
+    public void deleteOrderById(@PathVariable Long id,
+                                HttpServletResponse response) {
+        log.info("Deleting order by id: {}", id);
+        Optional<Order> order = ordersRepository.findById(id);
+
+        // Checking if order exists
+        if (order.isPresent()) {
+            // If exists then setting status to No Content (204)
+            response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            // And delete it from repo
+            ordersRepository.deleteById(id);
+        } else {
+            // Else set status to Not Found (404)
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            response.addHeader("message", "Order is not found");
+        }
+
     }
 }
