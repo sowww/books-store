9d2d85e1ef71bfd0331618b707e6732d826c49a3
Yuri Kulikov <sowww@yandex.ru>
2018-07-22 00:07:02 +0300

    Unnecessary queries deleted

--- src/main/java/com/example/spring/bookstore/db/user/UsersRepository.java
+++ src/main/java/com/example/spring/bookstore/db/user/UsersRepository.java
@@ -3,10 +3,6 @@ package com.example.spring.bookstore.db.user;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UsersRepository extends CrudRepository<User, Long> {
-    Optional<User> findByName(String name);
-    Optional<String> getNameById(Long id);
 }
